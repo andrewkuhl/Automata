5998eb45d72e1b577cb32fe7e941a6d996b4aa92
andrewkuhl <andrewkuhlz@gmail.com>
2022-04-05 10:14:33 -0700

    Initial Commit

---
+++ Automata/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Automata
+//
+//  Created by Andrew Kuhl on 4/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
